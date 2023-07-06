0f23cc2c1cbabc9b0e7d6d3c544589acf2b85bef
NatpacanSribanhad <natpacan.s@kkumail.com>
2023-07-06 10:35:48 +0700

    test deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
+      <h2>Test deploy</h2>
       <p>
         This is an example site to demonstrate how to use{" "}
         <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
